257868c392f77a593763b157374c2b6c8b08315e
Anna Golasowska <anna.golasowska@grupawp.pl>
2023-02-27 22:19:59 +0100

    Setup

---
+++ config/config.example.ts
@@ -0,0 +1,7 @@
+// export const config = {
+//     dbHost: 'localhost',
+//     user: 'user',
+//     password: 'password',
+//     database: 'database',
+//     corsOrigin: 'http://localhost:3000',
+// }
---
+++ index.ts
@@ -0,0 +1,31 @@
+import express, {json, Router} from "express";
+import cors from 'cors';
+import "express-async-errors";
+import {config} from "./config/config";
+// import {handleError} from "./utils/errors";
+// import rateLimit from 'express-rate-limit';
+// import {adRouter} from "./routers/ad.router";
+
+const app = express();
+
+app.use(cors({
+    origin: config.corsOrigin,
+}));
+
+app.use(json());
+// app.use(rateLimit({
+//     windowMs: 5 * 60 * 1000, // 15 minutes
+//     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+// }));
+
+const router = Router();
+
+// router.use('/ad', adRouter);
+
+app.use('/api', router);
+
+// app.use(handleError);
+
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Listening on http://localhost:3001');
+});
