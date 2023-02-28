cf6825a92f881ae68d7e282d57dc722da697f2bd
Anna Golasowska <anna.golasowska@grupawp.pl>
2023-02-28 17:26:49 +0100

    Added error handling

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express, {json, Router} from "express";
 import cors from 'cors';
 import "express-async-errors";
 import {config} from "./config/config";
-// import {handleError} from "./utils/errors";
+import {handleError, ValidationError} from "./utils/errors";
 // import rateLimit from 'express-rate-limit';
 // import {adRouter} from "./routers/ad.router";
 
@@ -18,13 +18,17 @@ app.use(json());
 //     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 // }));
 
-const router = Router();
+// const router = Router();
 
 // router.use('/ad', adRouter);
 
-app.use('/api', router);
+// app.use('/api', router);
 
-// app.use(handleError);
+// app.get('/', async (req, res) => {
+//     throw new ValidationError('Dammmn');
+// });
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on http://localhost:3001');
