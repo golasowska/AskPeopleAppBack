218969bfa83e556ae6543558e234aefb2f5cf5ea
Anna Golasowska <anna.golasowska@grupawp.pl>
2023-04-04 18:13:56 +0200

    added error check to update method

--- routers/question.router.ts
+++ routers/question.router.ts
@@ -22,11 +22,14 @@ export const questionRouter = Router()
         res.json(question);
     })
     .patch('/:id', async (req, res) => {
-        const {body} : {body: AnswerChange} = req;
+        const {body}: { body: AnswerChange } = req;
         const question = await QuestionRecord.getOne(req.params.id);
         //
         if (question === null) {
             throw new ValidationError('Could not find the question.');
+        }
+        if (body.answerBody.length < 1) {
+            throw new ValidationError('Error occured ! Form was submitted without data !');
         }
         await question.update(body.answerBody);
         const updatedQuestion = await QuestionRecord.getOne(req.params.id);
